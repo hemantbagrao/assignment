28252f8c55e8508a7174527ce3c8143eda2250bf
System Administrator <root@iPortalMackBookPro.local>
2021-09-26 17:34:26 +0530

    optimization

--- src/components/withdraw.js
+++ src/components/withdraw.js
@@ -7,10 +7,19 @@ const Withdraw = (props) => {
     const [formErrors, setformErrors] = useState(false);
     const amountRef = useRef(null);
 
+    const checkBalance = () => {
+        const balance = props.transactions.reduce(function(acc,curr) {
+            return acc += curr.currency * curr.quantity
+        },0);
+        return balance;
+    }
     const calculateDenomination = (amount) => {
-        
+        console.log("transactions",props.transactions);
         const result = props.transactions.sort((a, b) => b.currency - a.currency).map(val => Number(val.currency));
+        console.log("result1",result);
         const divideBy = [...new Set(result)];
+        
+        console.log("divideBy",divideBy);
         let resultArray = [];
         divideBy.map(c => {
             let obj = { [c]: Math.floor(amount / c) }
@@ -22,7 +31,16 @@ const Withdraw = (props) => {
 
     const withdrawCurrency = () => {
         const amount = amountRef.current.value;
-        if (amount) calculateDenomination(amount);
+        const accountBalance = checkBalance();
+        console.log("accountBalance", accountBalance);
+        if (amount) {
+            if(accountBalance > amount) {
+                calculateDenomination(amount)
+            }
+            else {
+                alert("Low balance!!");
+            }
+        }
         else setformErrors(true)
 
     }
